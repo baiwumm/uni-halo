59e1d79cb52873660531dad700de81ed0476c627
白雾茫茫丶 <baiwumm@foxmail.com>
2025-07-03 11:30:29 +0800

    Merge remote-tracking branch 'up/main'

---
+++ config/token.config.js
@@ -0,0 +1,13 @@
+import {getAppConfigs} from "@/config/index";
+
+/** 配置博客的内容 */
+const UniHaloConfig = Object.freeze({
+
+	/** 基础请求域名：你的Halo博客基础域名 */
+	BASE_API: "https://baiwumm.com",
+	// BASE_API: "https://blog.xiaoxiaomo.cn",
+
+	systemToken: getAppConfigs()?.basicConfig?.tokenConfig?.personalToken,
+})
+
+export default UniHaloConfig;
